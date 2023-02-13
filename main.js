372b462b8882fa21967e160312781ee7a11ff05b
Hoàng Vũ <121605731+DuongXuanHoangVu@users.noreply.github.com>
2023-02-13 14:17:34 +0700

    complete

--- main.js
+++ main.js
@@ -2,18 +2,21 @@ let myLibrary = []
 const submitBtn = document.querySelector('.submit')
 const bookName = document.querySelector('input[name="book"]')
 const bookAuthor = document.querySelector('input[name="author"]')
+const bookPages = document.querySelector('input[name="pages"]')
 const bookList = document.querySelector('.book-list')
 
 bookName.focus()
 
-function Book(name, author) {
+function Book(name, author, pages) {
     this.name = name
     this.author = author
+    this.pages = pages
     this.isRead = false
     this.getData = function () {
         return {
             name,
             author,
+            pages,
             isRead: this.isRead
         }
     }
@@ -22,7 +25,8 @@ function Book(name, author) {
 function addBookToLibrary () {
     let name = bookName.value
     let author = bookAuthor.value
-    let newBook = new Book(name, author)
+    let pages = bookPages.value
+    let newBook = new Book(name, author, pages)
     return newBook.getData()
 }
 
@@ -33,6 +37,7 @@ function renderBooks() {
             <div class="book-info">
                 <h1>Tên sách: ${item.name}</h1>
                 <p>Tác giả: ${item.author}</p>
+                <p>Số trang: ${item.pages}</p>
                 <p>Trạng thái:${item.isRead ? "Đang đọc" : "Chưa đọc"}</p>
             </div>
             <div class="btns">
@@ -59,6 +64,7 @@ submitBtn.onclick = function () {
     renderBooks()
     bookName.value = ''
     bookAuthor.value = ''
+    bookPages.value = ''
     bookName.focus()
 }
 
